0d9eab34f76fc2dcbf5151e7ce19032a567e405b
ViniciusZanetti <58536337+ViniciusZanetti@users.noreply.github.com>
2021-11-12 14:47:59 -0300

    .

--- form-app/src/Components/FormularioCadastro/FormularioCadastro.jsx
+++ form-app/src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,5 +1,4 @@
-import { Typography } from "@material-ui/core";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuarios from "./DadosUsuarios";
@@ -7,32 +6,17 @@ import DadosUsuarios from "./DadosUsuarios";
 function FormularioCadastro({ aoEnviar, aoValidar }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
 
-  function proximo(){
+  const formulario = [
+    <DadosUsuarios aoEnviar={proximo} />,
+    <DadosPessoais aoEnviar={proximo} aoValidar={aoValidar} />,
+    <DadosEntrega />,
+  ];
+
+  function proximo() {
     setEtapaAtual(etapaAtual + 1);
   }
 
-  function FormularioAtual(etapa) {
-    switch (etapa) {
-      case 0:
-        return <DadosUsuarios aoEnviar={proximo} />;
-      case 1:
-        return <DadosPessoais aoEnviar={proximo} aoValidar={aoValidar}/>;
-      case 2:
-        return <DadosEntrega />;
-      default:
-        return <Typography>Erro ao selecionar formulário</Typography>;
-    }
-  }
-
-  return (
-    <>
-      {FormularioAtual(etapaAtual)}
-    </>
-  );
+  return <>{formulario[etapaAtual]}</>;
 }
 
-
-
 export default FormularioCadastro;
-
-
