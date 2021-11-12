7c4aaffde7535dc6d93fa9778ea9f66a1897614f
ViniciusZanetti <58536337+ViniciusZanetti@users.noreply.github.com>
2021-11-12 12:57:37 -0300

    switch case

--- form-app/src/Components/FormularioCadastro/DadosUsuarios.jsx
+++ form-app/src/Components/FormularioCadastro/DadosUsuarios.jsx
@@ -1,13 +1,17 @@
 import { TextField, Container, Button } from "@material-ui/core";
 import React from "react";
 
-function DadosUsuarios() {
+function DadosUsuarios({aoEnviar}) {
   return (
-    <form>
+    <form onSubmit={(event) => {
+      event.preventDefault();
+      aoEnviar();
+    }}>
       <TextField
         id="email"
         label="email"
         type="email"
+        required
         variant="outlined"
         margin="normal"
         fullWidth
@@ -16,6 +20,7 @@ function DadosUsuarios() {
         id="password"
         label="senha"
         type="password"
+        required
         margin="normal"
         variant="outlined"
         fullWidth
--- form-app/src/Components/FormularioCadastro/FormularioCadastro.jsx
+++ form-app/src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import { Typography } from "@material-ui/core";
+import React, {useState} from "react";
+import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuarios from "./DadosUsuarios";
 
 function FormularioCadastro({ aoEnviar, aoValidar }) {
+  const [etapaAtual, setEtapaAtual] = useState(0);
+
+  function proximo(){
+    setEtapaAtual(etapaAtual + 1);
+  }
+
+  function FormularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuarios aoEnviar={proximo} />;
+      case 1:
+        return <DadosPessoais aoEnviar={proximo} aoValidar={aoValidar}/>;
+      case 2:
+        return <DadosEntrega />;
+      default:
+        return <Typography>Erro ao selecionar formulário</Typography>;
+    }
+  }
+
   return (
     <>
-      <DadosPessoais aoEnviar={aoEnviar} aoValidar={aoValidar}/>
-      <DadosUsuarios />
+      {FormularioAtual(etapaAtual)}
     </>
   );
 }
+
+
+
 export default FormularioCadastro;
+
+
